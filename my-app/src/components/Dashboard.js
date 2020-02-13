e6eaaeb353f8fb9b50dfc7404fa8d1f9dade6f46
alexis villaraza <alexis.borja@gmail.com>
2020-02-13 13:57:56 -0700

    added components added buttons to dashboard

---
+++ my-app/src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Dashboard() => {
+
+    return (
+        <div>
+            <button>Strike</button>
+            <button>Ball</button>
+            <button>Foul</button>
+            <button>Hit</button>
+        </div>
+    )
+}
+
+export default Dashboard;
