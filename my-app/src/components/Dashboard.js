d891d62eede1f33d1a76ba96b6393bb085069a41
alexis villaraza <alexis.borja@gmail.com>
2020-02-13 14:06:56 -0700

    added onClick to buttons

--- my-app/src/components/Dashboard.js
+++ my-app/src/components/Dashboard.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
-function Dashboard() => {
+function Dashboard() {
+    const [strike, setStrike] = useState(0);
+    const [ball, setBall] = useState(0);
+    const [foul, setFoul] = useState(0);
+    const [hit, setHit] = useState(0);
+
+    const strikeButton = e => {
+        setStrike(strike + 1);
+    };
+
+    const ballButton = e => {
+        setBall(ball + 1);
+    };
+
+    const foulButton = e => {
+        setFoul(foul + 1);
+    };
+
+    const hitButton = e => {
+        setHit(hit + 1);
+    };
+
 
     return (
         <div>
-            <button>Strike</button>
-            <button>Ball</button>
-            <button>Foul</button>
-            <button>Hit</button>
+            <button onClick={strikeButton}>Strike</button>
+            <button onClick={ballButton}>Ball</button>
+            <button onClick={foulButton}>Foul</button>
+            <button onClick={hitButton}>Hit</button>
         </div>
     )
 }
